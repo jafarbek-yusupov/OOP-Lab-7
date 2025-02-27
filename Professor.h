ffc53d5ff722a11bad26bc157208bd5306241084
j.yusupov <j.yusupov@newuu.uz>
2025-02-27 11:41:26 +0500

    2drcoup

--- Professor.h
+++ Professor.h
@@ -1,4 +1,21 @@
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
 
-#endif //PROFESSOR_H
+#include <iostream>
+#include <string>
+#include "UniversityMember.h"
+
+class Professor : public UniversityMember{
+private: int ID; std::string department;
+public:
+    Student(std::string name, int Age, int ID, std::string department) : UniversityMember(name, Age), ID(ID), department(department) {};
+    void displayInfo override() {
+        std::cout << "Name: " << getName() << " | ID: " << ID << " | Department : " << department << " | Age: " << getAge() << std::endl;
+    }
+    int getID(){ return ID;}
+    std::string getDepartment(){ return department; }
+    void setID(int nID){ ID = nID;}
+    void setDepartment(std::string nDep){ department = nDep;}
+};
+
+#endif
